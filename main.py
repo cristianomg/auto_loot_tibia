8e9b34c4815dbe04c32ac45ba9b54b1700c58ad5
Cristiano Macedo <cristianomacedo@MacBook-Air-de-Cristiano.local>
2022-09-11 01:43:28 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from pynput import keyboard
+import pyautogui
+from utils import load_positions, write_position
+
+BOT_DELAY = 0.01
+pyautogui.PAUSE = BOT_DELAY
+
+def auto_loot():
+    POSITIONS = load_positions()
+
+    for pos in POSITIONS:
+        pyautogui.click(button='right', x=pos['x'], y=pos['y'])
+
+def on_press(key):
+    try:
+        if key.char == 'r':
+            auto_loot()
+        elif key.char == 't':
+            set_position()
+    except:
+        pass
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+def set_position():
+    position = pyautogui.position()
+    write_position({'x':position.x, 'y': position.y})
+
+def main():
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,18 @@
+POSITIONS_FILE_NAME = 'positions.txt'
+
+def write_position(position):
+    f = open(POSITIONS_FILE_NAME, 'a')
+    f.write('{},{}\n'.format(position['x'], position['y']))
+    f.close()
+
+def load_positions():
+    f = open(POSITIONS_FILE_NAME, 'r')
+    positions = f.read().split('\n')
+    return [split_position(x_pos_y_pos) for x_pos_y_pos in positions if has_separator(x_pos_y_pos)]
+
+def split_position(x_pos_y_pos):
+    x, y = x_pos_y_pos.split(',')
+    return {'x': int(x), 'y': int(y)}
+
+def has_separator(str):
+    return True if ',' in str else False
