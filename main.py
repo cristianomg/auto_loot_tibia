b50a963925194ad7e570ca2c80ac0d2b01f67a86
Cristiano Macedo <cristianomacedo@MacBook-Air-de-Cristiano.local>
2022-09-11 01:59:49 -0300

    alter hotkey to set positions and add shuffle in position array

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from pynput import keyboard
 import pyautogui
 from utils import load_positions, write_position
+import random
 
-BOT_DELAY = 0.01
+BOT_DELAY = 0.05
 pyautogui.PAUSE = BOT_DELAY
 
 def auto_loot():
     POSITIONS = load_positions()
+
+    random.shuffle(POSITIONS)
 
     for pos in POSITIONS:
         pyautogui.click(button='right', x=pos['x'], y=pos['y'])
@@ -15,14 +18,15 @@ def on_press(key):
     try:
         if key.char == 'r':
             auto_loot()
-        elif key.char == 't':
-            set_position()
+
     except:
         pass
 
 def on_release(key):
     if key == keyboard.Key.esc:
         return False
+    elif key == keyboard.Key.f12:
+        set_position()
 
 def set_position():
     position = pyautogui.position()
